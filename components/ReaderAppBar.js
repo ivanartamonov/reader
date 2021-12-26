3ebdb96ed3b13b07cd121fd4a2ee989d9121cbc3
Ivan Artamonov <i.o.artamonov@gmail.com>
2021-12-26 20:34:51 +0200

    Create ReaderAppBar skeleton

---
+++ components/ReaderAppBar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {BookmarkBorder, FormatListBulleted, MoreVert, Settings} from "@mui/icons-material";
+
+const ReaderAppBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar
+                position={"static"}
+                color={'white'}
+                sx={{
+                    boxShadow: 'none',
+                    borderBottom: '1px solid #f1f1f1'
+                }}>
+                <Toolbar>
+                    <IconButton
+                        size="medium"
+                        edge="start"
+                        color="inherit"
+                        aria-label="back"
+                        sx={{ mr: 2 }}
+                    >
+                        <ArrowBackIcon />
+                    </IconButton>
+
+                    <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
+                        Глава 2. Рубиновый город
+                    </Typography>
+
+                    <IconButton
+                        size="medium"
+                        color="inherit"
+                        aria-label="To library"
+                        sx={{ ml: 1 }}
+                    >
+                        <BookmarkBorder />
+                    </IconButton>
+                    <IconButton
+                        size="medium"
+                        color="inherit"
+                        aria-label="Chapters"
+                        sx={{ ml: 1 }}
+                    >
+                        <FormatListBulleted />
+                    </IconButton>
+                    <IconButton
+                        size="medium"
+                        color="inherit"
+                        aria-label="Settings"
+                        sx={{ ml: 1 }}
+                    >
+                        <Settings />
+                    </IconButton>
+                    <IconButton
+                        size="medium"
+                        color="inherit"
+                        aria-label="More"
+                        sx={{ ml: 1 }}
+                    >
+                        <MoreVert />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default ReaderAppBar;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
 import Head from 'next/head'
+import {Button, Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import ReaderAppBar from "../components/ReaderAppBar";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Reader</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#9C1658',
+                dark: '#82174B'
+            },
+            secondary: {
+                main: '#666',
+            },
+            white: {
+                main: '#fff',
+                contrastText: '#444',
+            },
+            pale: {
+                main: '#FCF4D9',
+                contrastText: '#383838',
+            }
+        },
+    });
 
-      <main>
-        <h1>Hello, world</h1>
-      </main>
-    </div>
+  return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="container">
+            <Head>
+                <title>Reader</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <ReaderAppBar />
+                <Container>
+                    <h1>Глава 2. Рубиновый город</h1>
+                    <p>Тут должен быть текст главы</p>
+                    <Button variant="contained">Example button</Button>
+                </Container>
+            </main>
+        </div>
+      </ThemeProvider>
   )
 }
