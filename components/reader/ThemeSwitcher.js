29573d6f1d010fdd30e06836cdae1dadd8d9df62
Ivan Artamonov <i.o.artamonov@gmail.com>
2022-01-08 17:25:23 +0200

    ReaderSettings: ThemeSwitcher styles

--- components/reader/ThemeSwitcher.js
+++ components/reader/ThemeSwitcher.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import styles from './reader_settings.module.scss';
 import {Box, Radio, Typography} from "@mui/material";
-import {ColorLens} from "@mui/icons-material";
+import {Check, ColorLens} from "@mui/icons-material";
 import {ThemeContext} from "../ThemeProvider";
 
 const ThemeSwitcher = () => {
@@ -32,14 +32,32 @@ const ThemeSwitcher = () => {
                         disableRipple
                         disableFocusRipple
                         disableTouchRipple
-                        sx = {{padding: 0, marginRight: '5px'}}
+                        sx = {{
+                            padding: 0,
+                            marginRight: '5px',
+
+                        }}
                         classes={styles.radio}
                         checkedIcon={
-                            <Box className={styles.icon}/>
+                            <Box className={styles.icon} sx={{
+                                background: '#fff',
+                                border: '1px solid',
+                                borderColor: '#f1f1f1',
+                                textAlign: 'center'
+                            }}>
+                                <Check sx={{
+                                    fontSize: '14px'
+                                }} />
+                            </Box>
                         }
                         icon={
-                            <Box className={styles.icon}/>
+                            <Box className={styles.icon} sx={{
+                                background: '#fff',
+                                border: '1px solid',
+                                borderColor: '#ccc'
+                            }} />
                         }
+                        checked={themeName === 'DEFAULT'}
                         onClick={() => changeTheme('DEFAULT')}
                     />
                     <Radio
@@ -49,38 +67,27 @@ const ThemeSwitcher = () => {
                         sx = {{padding: 0, marginRight: '5px'}}
                         classes={styles.radio}
                         checkedIcon={
-                            <Box className={styles.icon}/>
+                            <Box className={styles.icon} sx={{
+                                background: '#333',
+                                border: '1px solid',
+                                borderColor: '#666',
+                                textAlign: 'center'
+                            }}>
+                                <Check sx={{
+                                    fontSize: '14px',
+                                    color: '#fff'
+                                }} />
+                            </Box>
                         }
                         icon={
-                            <Box className={styles.icon}/>
+                            <Box className={styles.icon} sx={{
+                                background: '#333',
+                                border: '1px solid',
+                                borderColor: '#333'
+                            }} />
                         }
+                        checked={themeName === 'DARK'}
                         onClick={() => changeTheme('DARK')}
-                    />
-                    <Radio
-                        disableRipple
-                        disableFocusRipple
-                        disableTouchRipple
-                        sx = {{padding: 0, marginRight: '5px'}}
-                        classes={styles.radio}
-                        checkedIcon={
-                            <Box className={styles.icon}/>
-                        }
-                        icon={
-                            <Box className={styles.icon}/>
-                        }
-                    />
-                    <Radio
-                        disableRipple
-                        disableFocusRipple
-                        disableTouchRipple
-                        sx = {{padding: 0}}
-                        classes={styles.radio}
-                        checkedIcon={
-                            <Box className={styles.icon}/>
-                        }
-                        icon={
-                            <Box className={styles.icon}/>
-                        }
                     />
                 </Box>
             </Box>
